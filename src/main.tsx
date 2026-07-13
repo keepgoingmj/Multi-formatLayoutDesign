e0e8c547e438473be5ab391ea9a0d30758ff2e27
keepgoingmj <butfect@gmail.com>
2026-07-13 17:23:09 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
+import { createRoot } from "react-dom/client";
+import App from "./app/App.tsx";
+import RenderView from "./app/RenderView.tsx";
+import "./styles/index.css";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
+const params = new URLSearchParams(window.location.search);
+const Root = params.has("render") ? RenderView : App;
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+createRoot(document.getElementById("root")!).render(<Root />);
